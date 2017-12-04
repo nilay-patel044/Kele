3d7022009549d540a27be6a8b3ff7862bcf49c88
nilay-patel044 <nilaypatel044@gmail.com>
2017-12-04 00:55:02 -0500

    Kele-1 and 2 Completed

---
+++ lib/kele.rb
@@ -0,0 +1,14 @@
+require 'httparty'
+
+class Kele
+  include HTTParty
+
+  base_uri 'https://www.bloc.io/api/v1'
+
+  def initialize(email, password)
+    response = self.class.post("/sessions", body: { "email": email, "password": password })
+    @auth_token = response ['auth_token']
+    raise "Invalid Login Credentials" if response.code == 401
+    puts @auth_token
+  end
+end
